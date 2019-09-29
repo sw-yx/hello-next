1602332e8fa25feff4b887a5b7ba53d659e8c2c3
sw-yx <swyx@dontemail.me>
2019-09-29 18:39:44 -0400

    query

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -2,5 +2,5 @@ export default function handle(req, res) {
   console.log('body', req.body) // The request body
   console.log('query', req.query) // The url querystring
   console.log('cookies', req.cookies) // The passed cookies
-  res.json({ title: 'Hello World' })
+  res.json({ title: 'Hello World', ...req.query })
 }
